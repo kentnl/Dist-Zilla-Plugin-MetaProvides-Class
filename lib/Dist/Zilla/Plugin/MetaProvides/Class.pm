9ffa3e0aba738db71ff4fa60cca49050b84c152f
Kent Fredric <kentfredric@gmail.com>
2017-03-08 23:10:46 +1300

    v2.001001
     - Bugfix: Avoid test failures with -Ddefault_inc_excludes_dot
     - Remove usage of Test::KENTNL/dztest
    
     [Dependencies::Stats]
     - Dependencies changed since 2.001000, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1)
     - develop: +11 ↑3 -3 (recommends: +1, suggests: ↑2)
     - runtime: ↓1 -1 (recommends: +1)
     - test: +1 -1 (recommends: ↑1 ↓1)

--- lib/Dist/Zilla/Plugin/MetaProvides/Class.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Class.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Class;
 
-our $VERSION = '2.001001';
+our $VERSION = '2.001002';
 
 # ABSTRACT: Scans Dist::Zilla's .pm files and tries to identify classes using Class::Discover.
 
